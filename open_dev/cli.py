b7a3c468511a238cc0e50404f7c962b29624e3bc
8Baller <8ball030@gmail.com>
2022-08-20 21:35:30 +0100

    fixes: workflows

--- open_dev/cli.py
+++ open_dev/cli.py
@@ -2,13 +2,15 @@
 
 import click
 
+description ="A collection of tooling to enable open source development."
 
 @click.command()
 def main():
     """Main entrypoint."""
     click.echo("open_dev")
     click.echo("=" * len("open_dev"))
-    click.echo("A collection of tooling to enable open source development.")
+    click.echo(description)
+    click.echo("*" * len(description))
 
 
 if __name__ == "__main__":
